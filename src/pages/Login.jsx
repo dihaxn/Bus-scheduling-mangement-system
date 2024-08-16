5b894b87f24dd8a7ae5f810e7019c46fe78fb9a7
Codersauruse <shehandesk123@gmail.com>
2024-08-17 02:44:32 +0530

    solved some problem in  sorting in map.js

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,11 +33,14 @@ function Login() {
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <h2 className="text-center mb-4">Login</h2>
+          <h2 className="text-center mb-4">
+            Welcome Back! Login to your account
+          </h2>
+
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
               <label htmlFor="name" className="form-label">
-                Name
+                User Name
               </label>
               <input
                 type="text"
