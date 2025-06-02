6b13caa941049692764317a233e4d35de63c14a9
dihaxn <ihanlaknuka@gmail.com>
2025-06-02 11:52:28 +0530

    Change footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,11 @@
 // Header.jsx
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "./logo.gif"; // Your bus system logo
 
-export default function Header({ isAuthenticated, setIsAuthenticated }) {
-  const navigate = useNavigate();
-
-  const handleSignOut = () => {
-    setIsAuthenticated(false);
-    navigate("/login");
-  };
-
+export default function Header() {
   return (
       <div>
         <div className="pos-f-t">
@@ -51,39 +44,14 @@ export default function Header({ isAuthenticated, setIsAuthenticated }) {
               </Nav.Item>
             </Nav>
 
-            {/* Login/Signup or Profile dropdown */}
+            {/* Always show Login and Sign Up on the right side */}
             <div className="ml-auto d-flex align-items-center">
-              {!isAuthenticated ? (
-                  <>
-                    <Link to="/login" className="btn btn-secondary" style={{ marginRight: "5px" }}>
-                      Login
-                    </Link>
-                    <Link to="/signup" className="btn btn-secondary">
-                      Sign Up
-                    </Link>
-                  </>
-              ) : (
-                  <div className="btn-group" role="group">
-                    <button
-                        id="btnGroupDrop1"
-                        type="button"
-                        className="btn btn-secondary dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                    >
-                      Profile
-                    </button>
-                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
-                      <Link className="dropdown-item" to="/profile">
-                        Profile
-                      </Link>
-                      <a className="dropdown-item" href="#" onClick={handleSignOut}>
-                        Sign Out
-                      </a>
-                    </div>
-                  </div>
-              )}
+              <Link to="/login" className="btn btn-secondary" style={{ marginRight: "5px" }}>
+                Login
+              </Link>
+              <Link to="/signup" className="btn btn-secondary">
+                Sign Up
+              </Link>
             </div>
           </nav>
         </div>
